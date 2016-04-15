a05259df393cc5b1f859e0dd9814c0c942afb057
Joseph Thomas <joey6978@gmail.com>
2016-04-14 22:58:32 -0700

    Re-writing everything...

--- code.js
+++ code.js
@@ -1,36 +1,9 @@
 
 
-
 function main(data){
-	data	=	data.split('\n');
-	var T	=	parseInt(data.shift());
-	while(--T>0){
-		FL	=	data.shift().split(' ').map(parseFloat);
-		N	=	FL[0];
-		M	=	FL[1];
-		PATHS	=	[];
-		NEW_PATHS	=	[]
-		while(--N>0){
-			path	=	data.shift().substr(1).split('/');
-			PATHS.push(path);	
-		}
-		while(--M>0){
-			path	=	data.shift().substr(1).split('/');
-			NEW_PATHS.push(path);
-		}	
-		var fileTree	=	{};
-		var objPtr	=	{};
-		PATHS.forEach(function(path){
-			fileTree[path[0]]	=	{};
-			objPtr	=	fileTree[path[0]];
-			
-		});
-		
-		
-		
-		
-	}
 	
+	
+	return data;
 }
 
 //The input file that you want to run through... 
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var GET	=	{};
 var Sockets	=	{};
 var Input	=	{};
 var Ouput	=	"";
-
+var Analyzing	=	false;
 
 //A little handler for this server
 
@@ -44,78 +44,6 @@ io.on('connection',function(socket){
 	socket.emit('file',Input);
 	socket.emit('output',Ouput);
 });
-
-
-
-//Hijack the console.log
-(function(){
-    var oldLog = console.log;
-    console.log = function (msg) {
-			for(var id in Sockets){
-				Sockets[id].emit('console',msg);
-			}
-        oldLog.apply(console, arguments);
-    };
-})();
-
-
-
-
-
-
-(function main(){
-	//Read the config file.
-	var config	=	require('./config');
-
-	//Load the input.. and store in input file
-	Input	=	{
-		name	:	config.inputFile,
-		data	:	fs.readFileSync(config.inputFile).toString('ascii')
-	};
-	
-	//Calculate an output
-	try{
-		var code	=	require(config.codeFile);
-		Ouput	=	code(Input.data);
-	}catch(e){
-		console.log(e);
-		Ouput	=	JSON.stringify(e);
-	}
-	
-	//Watch for changes in the code, and update the output
-	var codeFile	=	chokidar.watch(config.codeFile);
-	codeFile.on('change',function(path){
-		//delete the require for the input
-		delete require.cache[require.resolve(config.codeFile)];
-		try{
-			var code	=	require(config.codeFile);
-			Ouput	=	code(Input.data);
-		}catch(e){
-			console.log(e);
-			Output	=	JSON.stringify(e);
-		}
-		//send the output to everyone
-		for(var socket in Sockets){
-			Sockets[socket].emit('output',Ouput);
-		}
-	});
-	
-	//Watch for changes in the input file, and update it
-	var inputFile	=	chokidar.watch(config.inputFile);
-	inputFile.on('change',function(paht){
-		Input	=	{
-			name	:	config.inputFile,
-			data	:	fs.readFileSync(config.inputFile).toString('ascii')
-		};
-		//send the output to everyone
-		for(var socket in Sockets){
-			Sockets[socket].emit('input',Input);
-		}
-		
-	});
-	
-})();
-
 
 
 Server.listen(1997);
