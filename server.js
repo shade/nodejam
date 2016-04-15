25e7b0006c9183b826117f2d9359d65c87d96470
Joseph Thomas <joey6978@gmail.com>
2016-04-14 23:12:49 -0700

    Added a giant function to handle rechecking and broadcasting

--- server.js
+++ server.js
@@ -2,15 +2,13 @@ var Server	=	require('http').createServer(Handle);
 var io	=	require('socket.io')(Server);
 var fs	=	require('fs');
 var chokidar	=	require('chokidar');
+var Config	=	require('./config');
 
 var GET	=	{};
 var Sockets	=	{};
-var Input	=	{};
-var Ouput	=	"";
 var Analyzing	=	false;
 
 //A little handler for this server
-
 function Handle(req,res){
 	console.log(req.url)
 	req.url	=	req.url.trim().toLowerCase();
@@ -39,11 +37,80 @@ GET['/main.js']	=	GET['/play.png']	=	GET['/']	=	function(req,res){
 	});	
 	
 }
+
+//Handle the sockets
 io.on('connection',function(socket){
 	Sockets[socket.id]	=	socket;
 	socket.emit('file',Input);
 	socket.emit('output',Ouput);
+	reCheck();
 });
 
+//Handle our console.log
+(function(){
+    var oldLog = console.log;
+    console.log = function (message) {
+			if(Analyzing){
+        for(var id in Sockets){
+					Sockets[id].emit('console',message);		
+				}
+			}
+        oldLog.apply(console, arguments);
+    };
+})();
 
+
+//Handle file changes
+var watchers	=	{
+	input	:	chokidar.watch('./input.dat'),
+	code	:	chokidar.watch('./code.js')
+};
+
+
+
+
+
+
+//The function to make sure everything works.
+function reCheck(){
+	
+	//Tell everyone we're re-analyzing the code
+	for(id in Sockets){
+		Sockets[id].emit('new');
+	}
+	
+	//So that console.log knows to record this.
+	Analyzing	=	true;
+	
+	
+	//Begin code re-analysis
+	var _output	=	"";
+	delete require.cache[require.resolve(Config.codeFile)];
+	try{
+		_output	=	require(Config.codeFile)(fs.readFileSync(Config.inputFile).toString('ascii'))
+	}catch(err){
+		_output	=	err;
+	}
+	
+	//So console.log knows we're done
+	Analyzing	=	false;
+	
+	//Tell everyone the results...
+	for(id in Sockets){
+		Sockets[id].emit('output',_output);
+	}
+	
+	
+	//Tell everyone we're done
+	for(id in Sockets){
+		Sockets[id].emit('done');
+	}
+}
+
+
+
+
+
+
+//Start the server.
 Server.listen(1997);
